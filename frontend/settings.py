9daae5dbaba79a36ba137890cfd59b53f1b2f4ec
Harinand2024 <harinand@markaziasolutions.com>
2025-06-21 18:54:14 +0530

    ui change

--- frontend/settings.py
+++ frontend/settings.py
@@ -128,6 +128,11 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8001",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",  # frontend port
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
